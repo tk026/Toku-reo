a4ced7957890e07f2ec1b34cd056442ea813c006
tk026 <TK026@atcltd.atc.org.nz>
2017-02-27 11:41:18 +1300

    Added Navigation page

---
+++ navigation.php
@@ -0,0 +1,30 @@
+<div class="header-koru">
+	<nav class="navbar navbar-default">
+	    <div class="container">
+	        <a class="navbar-brand" href="index.html">Toku Reo</a>
+	        <div class="navbar-header">
+	            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+	                <span class="icon-bar"></span>
+	                <span class="icon-bar"></span>
+	                <span class="icon-bar"></span>
+	            </button>
+	        </div>
+
+	        <div class="collapse navbar-collapse" id="myNavBar">
+
+	        <ul class="nav navbar-nav navbar-right">
+	        	<li class="<?php if($currentPage =='index'){echo 'active';}?>"><a href="/"> Toku reo </a></li>"></li>
+
+	        	<li class="<?php if($currentPage =='mihi'){echo 'active';}?>"><a href="/Toku-reo/mihi.php"> Toku reo </a></li>"></li>
+
+	        </ul>
+
+	       </div>
+
+	      </div>
+	     </nav>
+	    <div class="hero">
+	    	<h1> Toku Reo - My Language </h1>
+	    	<p> This is a place to learn basic phrases and sayings in the Maori language </p>
+	    	<!-- <div class="btn btn-primary"> -->
+	    </div>
