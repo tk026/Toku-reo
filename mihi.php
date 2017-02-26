d1c3e9c28cda8fa27028dba068b220bd103aa9cf
tk026 <TK026@atcltd.atc.org.nz>
2017-02-27 12:01:24 +1300

    added image wahine

--- mihi.php
+++ mihi.php
@@ -4,8 +4,13 @@
 	include 'navigation.php';
 ?>
 <div class="container">
-<p> Toku reo , Toku Oho Oho <br>
-Toku reo, <br>
-Toku Mapihi maurea </p>
-<img src="wahine.jpg">
+<p> Nga Mihi - Greetings and sayings<br>
+
+Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+<img src="img/wahine.jpg">
 </div>
