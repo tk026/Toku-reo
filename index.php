ec22317f4b481e13b8703beb6f9341d844af6d7a
tk026 <TK026@atcltd.atc.org.nz>
2017-02-27 10:48:20 +1300

    add header page

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+	$currentPage = 'index';// current page is index, do the same for other page
+	?>
+
+<div class="container">
+<p> Toku reo , Toku Oho Oho <br>
+Toku reo, <br>
+Toku Mapihi maurea </p>
+<img src="wahine.jpg">
+
